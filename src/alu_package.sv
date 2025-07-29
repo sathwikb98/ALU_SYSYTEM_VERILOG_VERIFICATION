638df55bd8d2dc9989d6d46de3f304f6e0a5961e
Sathwik Bhat <109812189+sathwikb98@users.noreply.github.com>
2025-07-29 16:30:45 +0530

    Create alu_package.sv

---
+++ src/alu_package.sv
@@ -0,0 +1,11 @@
+package alu_package;
+  `include "defines.svh"
+  `include "alu_transaction.sv"
+  `include "alu_generator.sv"
+  `include "alu_driver.sv"
+  `include "alu_monitor.sv"
+  `include "alu_reference_model.sv"
+  `include "alu_scoreboard.sv"
+  `include "alu_environment.sv"
+  `include "alu_test.sv"
+endpackage
